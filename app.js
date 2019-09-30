aecebdd6f67b4d30048988cc382e48c6edd80610
Shahzad <alim6077@gmail.com>
2019-09-30 11:18:09 +0500

    install express

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-sample.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours
+    }
+  });
+});
 
 const port = 8011;
 app.listen(port, () => {
