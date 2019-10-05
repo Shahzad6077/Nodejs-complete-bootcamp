19bcd04ae839217494afb1a045ab7099ba44b057
Shahzad <alim6077@gmail.com>
2019-10-05 12:53:20 +0500

    DEFINE USER ROUTE

--- app.js
+++ app.js
@@ -119,9 +119,40 @@ const deleteTour = (req, res) => {
   }
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined yet"
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined yet"
+  });
+};
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined yet"
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined yet"
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined yet"
+  });
+};
 /*
  * 3) ROUTES
  */
+
 app
   .route("/api/v1/tours")
   .get(getAllTours)
@@ -131,6 +162,16 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app
+  .route("/api/v1/users")
+  .get(getAllUsers)
+  .post(createUser);
+app
+  .route("/api/v1/users/:id")
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 /*
  * 4) START SERVERR
